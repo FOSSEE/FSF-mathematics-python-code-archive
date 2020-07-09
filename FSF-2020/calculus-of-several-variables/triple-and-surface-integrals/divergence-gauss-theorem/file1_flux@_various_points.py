a78063c421876d66fd1c1750a5a5e87cdd5274ca
vishal786-commits <66894871+vishal786-commits@users.noreply.github.com>
2020-07-09 08:55:35 +0530

    Update file1_flux@_various_points.py

--- FSF-2020/calculus-of-several-variables/triple-and-surface-integrals/divergence-gauss-theorem/file1_flux@_various_points.py
+++ FSF-2020/calculus-of-several-variables/triple-and-surface-integrals/divergence-gauss-theorem/file1_flux@_various_points.py
@@ -2,11 +2,11 @@ from manimlib.imports import *
 def pendulum_vector_field_func(point):
     #theta, omega = point[:2]
     return np.array([
-        5*point[2]+point[1],
-        3*point[0]+3*point[1],
+        5*point[0]+point[1],
+        3*point[1]+3*point[1],
         0,
     ])
-class SimpleField(Scene):
+class SF(Scene):
     CONFIG = {
         #"func": cylinder_flow_vector_field,
         "flow_time": 5,
@@ -20,13 +20,30 @@ class SimpleField(Scene):
         # plane = NumberPlane(color=RED)
         # plane.add(plane.get_axis_labels()) 
         # self.add(plane)  
-        # self.initialize_vector_field()
 
-        c1=Circle(radius=3,color=RED)
+        A=TextMobject("The net flux through the green circular region is zero",tex_to_color_map={"green": GREEN})
+        B=TextMobject("The net flux through the blue circular region is non-zero",tex_to_color_map={"blue": BLUE})
+
+        c1=Circle(color=GREEN, radius=1.5)
+        c1.shift(4*LEFT+2.2*UP)
+        c2=Circle(color=BLUE, radius=1.5)
+        
+
+
+
+        self.play(ShowCreation(A))
+        self.wait(0.5)
+        self.play(ApplyMethod(A.shift, (0.8*UP+0.2*LEFT)))
+        self.play(ShowCreation(B))
+        # self.play(ApplyMethod(B.shift, (2*UP)))
+        self.wait(2)
+        self.play(FadeOut(A),FadeOut(B))
+        self.initialize_vector_field()
         field = self.vector_field
-        self.play(c1)
-        self.play(ShowCreation(field), run_time=10)
-        self.wait(3)
+        self.play(ShowCreation(field), run_time=4)
+        self.play(ShowCreation(c1))
+        self.play(ShowCreation(c2))
+        self.wait(1)
         lines = StreamLines(
             pendulum_vector_field_func,
             virtual_time=3,
