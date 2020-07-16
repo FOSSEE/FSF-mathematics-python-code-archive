3fd2d95a0e3c5f15cb3c9103f715a7f51055db28
Archit Sangal <archit.sangal@iiitb.org>
2020-07-16 17:10:30 +0530

    Archit Sangal

--- FSF-2020/linear-algebra/linear-transformations/Linear-Transformations-(Linear-Maps)/file1_transformations.py
+++ FSF-2020/linear-algebra/linear-transformations/Linear-Transformations-(Linear-Maps)/file1_transformations.py
@@ -6,12 +6,16 @@ class text(Scene):
         text1.scale(0.9)
         text2 = TextMobject("must either remain straight lines or sends to a point in the grid formed")
         text2.scale(0.9)
+        text3 = TextMobject("Origin must remains where it was before transformation.")
+        text3.scale(0.9)
         text1.move_to(ORIGIN+UP)
         text2.move_to(ORIGIN)
+        text3.move_to(ORIGIN+DOWN)
         self.play(Write(text1))
         self.play(Write(text2))
+        self.play(Write(text3))
         self.wait()
-        self.play(FadeOut(text1),FadeOut(text2))
+        self.play(FadeOut(text1),FadeOut(text2),FadeOut(text3))
 
 class LinearTransformation(LinearTransformationScene):
     CONFIG = {
@@ -44,3 +48,26 @@ class NonLinearTransformation(Scene):
         text.add_background_rectangle()
         self.play(Write(text))
         self.wait()
+        
+class MoveOrigin(LinearTransformationScene):
+
+    CONFIG = {
+        "show_basis_vectors": False,
+    }
+    def construct(self):
+        self.wait()
+
+        dot = Dot(ORIGIN, color = YELLOW)
+        self.add_transformable_mobject(dot)
+        self.apply_nonlinear_transformation(self.func)
+        text = TextMobject("This is also not a linear transformation as the origin moves from its original position")
+        text.move_to(2*DOWN)
+        text.scale(0.5)
+        text.set_color(YELLOW)
+        text.add_background_rectangle()
+        self.play(Write(text))
+        self.wait()
+
+    def func(self, point):
+        matrix_transform = self.get_matrix_transformation([[1, -1], [1, 1]])
+        return matrix_transform(point) + UP+ RIGHT
